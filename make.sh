f3c46015ee3feab219d1c7079939b74373ef0467
Thomas Ebert <thomas.ebert@te-online.net>
2018-03-07 22:32:07 +0100

    Optimize make bash script to optimize the workflow of releasing apps.

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
-echo -n "Enter the version (like 1.0.0) and press [ENTER]: "
+echo "Enter the version (like 1.0.0) and press [ENTER]: "
 read VERSION
 
-echo -n "Enter appname and press [ENTER]: "
+echo "Enter appname and press [ENTER]: "
 read APPNAME
 
 DIRNAME=$(pwd)
@@ -10,4 +10,11 @@ DIRNAME=$(pwd)
 tar -czf $DIRNAME/$APPNAME/v$VERSION/$APPNAME.tar.gz -C $DIRNAME/$APPNAME/v$VERSION $APPNAME
 
 # Create signature and copy to clipboard
-openssl dgst -sha512 -sign ~/.nextcloud/certificates/$APPNAME.key $DIRNAME/$APPNAME/v$VERSION/$APPNAME.tar.gz | openssl base64 | pbcopy
+SIGNATURE=$(openssl dgst -sha512 -sign ~/.nextcloud/certificates/$APPNAME.key $DIRNAME/$APPNAME/v$VERSION/$APPNAME.tar.gz | openssl base64)
+
+echo "https://github.com/te-online/nextcloud-app-releases/raw/master/$APPNAME/v$VERSION/$APPNAME.tar.gz" | pbcopy
+echo "🌍  The release URL for this release should be in your clipboard now. Press [ENTER] to continue."
+read DUMMY
+
+echo $SIGNATURE | pbcopy
+echo "🎉  The signature for submitting the app is now in your clipboard."
