51e246e6fe528254836ccd705577179440e9cc37
Bot <mail@te-online.net>
2020-06-22 23:33:23 +0200

    Add release v0.1.2 of app timemanager.

---
+++ timemanager/v0.1.2/timemanager/db/timemapper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OCA\TimeManager\Db;
+
+use OCP\AppFramework\Db\Mapper;
+use OCP\IDBConnection;
+
+/**
+ * Class TimeMapper
+ *
+ * @package OCA\TimeManager\Db
+ * @method Time insert(Time $entity)
+ */
+class TimeMapper extends ObjectMapper {
+	public function __construct(IDBConnection $db, CommitMapper $commitMapper) {
+		parent::__construct($db, $commitMapper, 'timemanager_time');
+	}
+
+	public function deleteByTaskId($uuid, $commit) {
+		$times = $this->getObjectsByAttributeValue('task_uuid', $uuid);
+		foreach($times as $time) {
+			$time->setCommit($commit);
+			$time->setChanged(date('Y-m-d H:i:s'));
+			$time->setStatus('deleted');
+			$this->update($time);
+		}
+	}
+
+	public function deleteChildrenForEntityById($uuid, $commit) {
+		// Do nothing here, because times have no children.
+	}
+}
