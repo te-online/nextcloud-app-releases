86b1af1433d8df265b8945dc42a2b2619f36c9a7
Bot <mail@te-online.net>
2021-12-04 00:24:20 +0100

    Add release v0.2.7 of app timemanager.

---
+++ timemanager/v0.2.7/timemanager/db/commit.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OCA\TimeManager\Db;
+
+use OCP\AppFramework\Db\Entity;
+use OCA\TimeManager\Helper\ISODate;
+
+/**
+ * Class Item
+ *
+ * @package OCA\TimeManager\Db
+ */
+class Commit extends Entity {
+	protected $commit;
+	protected $created;
+	protected $userId;
+
+	/**
+	 * Creates an array that represents the item in array format
+	 *
+	 * @return array item representation as array
+	 */
+	function toArray() {
+		return [
+			"commit" => $this->getCommit(),
+			"created" => ISODate::convert($this->getCreated()),
+		];
+	}
+
+	/**
+	 * Creates a string from the commit
+	 *
+	 * @return array item representation as array
+	 */
+	function toString() {
+		return $this->getCommit();
+	}
+}
