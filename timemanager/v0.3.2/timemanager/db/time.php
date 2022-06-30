a9c69ea8cf2e0e8fa2db6b8c3050f36658be1001
Bot <mail@te-online.net>
2022-06-30 23:24:04 +0200

    Add release v0.3.2 of app timemanager.

---
+++ timemanager/v0.3.2/timemanager/db/time.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace OCA\TimeManager\Db;
+
+use OCP\AppFramework\Db\Entity;
+use OCA\TimeManager\Helper\ISODate;
+
+/**
+ * Class Item
+ *
+ * @package OCA\TimeManager\Db
+ * @method string getName()
+ * @method void setName(string $value)
+ * @method string getUuid()
+ * @method void setUuid(string $value)
+ * @method string getNote()
+ * @method void setNote(string $value)
+ * @method string getUserId()
+ * @method void setUserId(string $value)
+ */
+class Time extends Entity {
+	protected $changed;
+	protected $created;
+	protected $start;
+	protected $end;
+	protected $taskUuid;
+	protected $note;
+	protected $uuid;
+	protected $commit;
+	protected $userId;
+	protected $status;
+	protected $paymentStatus;
+
+	/**
+	 * Creates an array that represents the item in array format
+	 *
+	 * @return array item representation as array
+	 */
+	function toArray() {
+		return [
+			"changed" => ISODate::convert($this->getChanged()),
+			"commit" => $this->getCommit(),
+			"created" => ISODate::convert($this->getCreated()),
+			"end" => ISODate::convert($this->getEnd()),
+			"note" => $this->getNote(),
+			"paymentStatus" => $this->getPaymentStatus(),
+			"start" => ISODate::convert($this->getStart()),
+			"task_uuid" => $this->getTaskUuid(),
+			"uuid" => $this->getUuid(),
+		];
+	}
+
+	/**
+	 * Gets the start date formatted.
+	 *
+	 * @return string
+	 */
+	function getStartFormatted($format = "D, j. F Y") {
+		$start = date_create($this->getStart());
+		return date_format($start, $format);
+	}
+
+	/**
+	 * Gets the start date localized
+	 *
+	 * @param string $width The format name; it can be 'full' (eg 'EEEE, MMMM d, y' - 'Wednesday, August 20, 2014'), 'long' (eg 'MMMM d, y' - 'August 20, 2014'), 'medium' (eg 'MMM d, y' - 'August 20, 2014') or 'short' (eg 'M/d/yy' - '8/20/14')
+	 * @return string
+	 */
+	function getStartLocalized($width = "full") {
+		$l = \OC::$server->getL10N("timemanager");
+		return $l->l("date", $this->getStart(), ["width" => $width]);
+	}
+
+	/**
+	 * Gets the end date formatted.
+	 *
+	 * @return string
+	 */
+	function getEndFormatted($format = "D, j. F Y") {
+		$end = date_create($this->getEnd());
+		return date_format($end, $format);
+	}
+
+	/**
+	 * Gets the duration formatted.
+	 *
+	 * @return string
+	 */
+	function getDurationInHours() {
+		if ($this->status === "deleted") {
+			return 0;
+		}
+
+		$start = date_create($this->getStart());
+		$end = date_create($this->getEnd());
+		$diff = date_diff($start, $end);
+		return round($diff->format("%h%") + $diff->format("%i%") / 60, 2);
+	}
+}
---
+++ timemanager/v0.3.2/timemanager/js/views/Statistics.svelte
@@ -0,0 +1,291 @@
+<script>
+	export let statsApiUrl;
+	export let requestToken;
+	export let controls = true;
+	export let includeShared = false;
+
+	import { onMount } from "svelte";
+	import {
+		startOfWeek,
+		addDays,
+		startOfDay,
+		endOfDay,
+		format,
+		isSameDay,
+		startOfToday,
+		getWeek,
+		addWeeks,
+		subWeeks,
+		getYear,
+		endOfWeek,
+		isDate,
+		parse,
+		intervalToDuration,
+		addMonths,
+		differenceInDays,
+		differenceInWeeks,
+		differenceInMonths,
+		differenceInYears,
+		addYears
+	} from "date-fns";
+	import { translate } from "@nextcloud/l10n";
+	import { Helpers } from "../lib/helpers";
+
+	const simpleRounding = number => Math.round(number * 100) / 100;
+
+	const localeOptions = Helpers.getDateLocaleOptions();
+	const dateFormat = "yyyy-MM-dd";
+
+	export let start = format(startOfWeek(new Date(), localeOptions), dateFormat, new Date());
+	export let end = format(endOfWeek(new Date(), localeOptions), dateFormat, new Date());
+
+	$: loading = false;
+	$: scale = "day";
+	$: points = [];
+	$: weekTotal = 0;
+	$: todayTotal = 0;
+	$: highest = 0;
+	$: startCursor = isDate(parse(start, dateFormat, new Date()))
+		? parse(start, dateFormat, new Date())
+		: startOfWeek(new Date(), localeOptions);
+	$: endCursor = isDate(parse(end, dateFormat, new Date()))
+		? parse(end, dateFormat, new Date())
+		: endOfWeek(new Date(), localeOptions);
+	$: currentWeek = null;
+	const updateWeek = () => {
+		weekTotal = 0;
+		todayTotal = 0;
+		highest = 0;
+		currentWeek = getWeek(startCursor, localeOptions);
+	};
+
+	onMount(async () => {
+		updateWeek();
+		loadData();
+	});
+
+	const loadData = async () => {
+		loading = true;
+		// Reset points
+		points = [];
+		// Determine duration between cursors
+		const durationDays = differenceInDays(endCursor, startCursor);
+		const durationMonths = differenceInMonths(endCursor, startCursor);
+		const durationWeeks = differenceInWeeks(endCursor, startCursor);
+		const durationYears = differenceInYears(endCursor, startCursor);
+		// Determine scale
+		if (durationDays > 31 && durationDays <= 180) {
+			scale = "week";
+			Array.from(Array(durationWeeks + 1).keys()).forEach(week => {
+				points.push({
+					date: addWeeks(startCursor, week)
+				});
+			});
+		} else if (durationDays > 180 && durationMonths <= 24) {
+			scale = "month";
+			Array.from(Array(durationMonths + 1).keys()).forEach(month => {
+				points.push({
+					date: addMonths(startCursor, month)
+				});
+			});
+		} else if (durationMonths > 24) {
+			scale = "year";
+			Array.from(Array(durationYears + 1).keys()).forEach(year => {
+				points.push({
+					date: addYears(startCursor, year)
+				});
+			});
+		} else {
+			scale = "day";
+			Array.from(Array(durationDays + 1).keys()).forEach(day => {
+				points.push({
+					date: addDays(startCursor, day)
+				});
+			});
+		}
+
+		// Load data from API
+		const { grouped, js_date_format } = await loadStats();
+
+		// Extract points from grouped array
+		points = points.map(point => {
+			// Get total from API response
+			const total = grouped[format(point.date, js_date_format)];
+			point.stats = {
+				total: total ? simpleRounding(total) : 0
+			};
+			// Find highest value
+			if (total > highest) {
+				highest = total;
+			}
+			// Sum up total
+			weekTotal += point.stats.total;
+			// Day total
+			if (isSameDay(point.date, startOfToday())) {
+				todayTotal += point.stats.total;
+			}
+
+			return point;
+		});
+
+		// Set columns
+		document.documentElement.style.setProperty("--tm-stats-columns", points.length);
+
+		loading = false;
+	};
+
+	const loadStats = async () => {
+		const start = format(startOfDay(startCursor), "yyyy-MM-dd HH:mm:ss");
+		const end = format(endOfDay(endCursor), "yyyy-MM-dd HH:mm:ss");
+		let statUrl = `${statsApiUrl}?start=${start}&end=${end}&group_by=${scale}&shared=${includeShared ? 1 : 0}`;
+
+		// Parse current URL for filters
+		const urlParts = document.location.href.split("?");
+		if (urlParts.length > 1) {
+			const queryString = urlParts[1];
+			const queryStringParts = queryString.split("&");
+			let queryStringVariables = {};
+			// Map over all query params
+			queryStringParts.forEach(part => {
+				// Split query params
+				const partParts = part.split("=");
+				const [name, value] = partParts;
+				// Apply filters from query params
+				if (name === "status" && value) {
+					statUrl += `&status=${value}`;
+				}
+				if (name === "tasks" && value && value.length) {
+					statUrl += `&tasks=${value}`;
+				}
+				if (name === "projects" && value && value.length) {
+					statUrl += `&projects=${value}`;
+				}
+				if (name === "clients" && value && value.length) {
+					statUrl += `&clients=${value}`;
+				}
+				if (name === "userFilter" && value && value.length) {
+					statUrl += `&userFilter=${value}`;
+				}
+			});
+		}
+
+		const stats = await fetch(statUrl, {
+			method: "GET",
+			headers: {
+				requesttoken: requestToken,
+				"content-type": "application/json"
+			}
+		});
+		return await stats.json();
+	};
+
+	const weekNavigation = mode => {
+		if (mode === "reset") {
+			startCursor = startOfWeek(startOfToday(), localeOptions);
+			endCursor = endOfWeek(startCursor, localeOptions);
+		} else if (mode === "next") {
+			startCursor = addWeeks(startCursor, 1);
+			endCursor = addWeeks(endCursor, 1);
+		} else {
+			startCursor = subWeeks(startCursor, 1);
+			endCursor = subWeeks(endCursor, 1);
+		}
+		updateWeek();
+		loadData();
+	};
+
+	const formatDateForScale = (date, type) => {
+		if (type === "primary") {
+			if (scale === "year") {
+				return format(date, "yyyy", localeOptions);
+			}
+			if (scale === "month") {
+				return format(date, "LLL", localeOptions);
+			}
+			if (scale === "week") {
+				return `${translate("timemanager", "Week")} ${format(date, "w", localeOptions)}`;
+			}
+			return format(date, "iii", localeOptions);
+		}
+		if (type === "secondary") {
+			if (scale === "year") {
+				return "";
+			}
+			if (scale === "month") {
+				return format(date, "yyyy", localeOptions);
+			}
+			if (scale === "week") {
+				return `${format(startOfWeek(date, localeOptions), "d.M.", localeOptions)} - ${format(
+					endOfWeek(date, localeOptions),
+					"d.M.",
+					localeOptions
+				)}`;
+			}
+			return format(date, "d.M.", localeOptions);
+		}
+	};
+</script>
+
+{#if controls}
+	<h2>{translate('timemanager', 'Statistics')}</h2>
+{/if}
+<div class={`${loading ? 'icon-loading' : ''}`}>
+	{#if controls}
+		<div class="top-stats">
+			<figure>
+				<figcaption class="tm_label">{translate('timemanager', 'Today')}</figcaption>
+				{simpleRounding(todayTotal)} {translate('timemanager', 'hrs.')}
+			</figure>
+			<figure>
+				<figcaption class="tm_label">{translate('timemanager', 'Week')}</figcaption>
+				{simpleRounding(weekTotal)} {translate('timemanager', 'hrs.')}
+			</figure>
+		</div>
+	{/if}
+	<div class="graphs">
+		<div class={`hours-per-week ${points.length > 12 || window.clientWidth < 768 ? 'many' : 'few'}`}>
+			{#if !loading && weekTotal > 0}
+				{#each points as point, index}
+					<div class="column">
+						{#if point && point.stats}
+							{#if point.stats.total > 0}
+								<span class="hours-label">{point.stats.total} {translate('timemanager', 'hrs.')}</span>
+								<div class="column-inner" style={`height: ${(point.stats.total / highest) * 100}%`} />
+							{/if}
+							<div class="date-label">
+								<span class="day">{formatDateForScale(point.date, 'primary')}</span>
+								<span class="date">{formatDateForScale(point.date, 'secondary')}</span>
+							</div>
+						{/if}
+					</div>
+				{/each}
+			{/if}
+			{#if controls && !loading && weekTotal === 0}
+				<p class="empty">{translate('timemanager', 'When you add entries for this week graphs will appear here.')}</p>
+			{/if}
+		</div>
+		{#if controls}
+			<nav class="week-navigation">
+				<button class="previous" on:click|preventDefault={() => weekNavigation('previous')}>
+					{translate('timemanager', 'Previous week')}
+				</button>
+				<span>
+					{translate('timemanager', 'Week')} {currentWeek}
+					<span class="dates">
+						({format(startOfWeek(startCursor, localeOptions), 'iiiiii d.MM.Y', localeOptions)} &ndash; {format(endOfWeek(startCursor, localeOptions), 'iiiiii d.MM.Y', localeOptions)})
+					</span>
+				</span>
+				<span>
+					{#if !isSameDay(startOfWeek(startOfToday(), localeOptions), startCursor)}
+						<button class="current" on:click|preventDefault={() => weekNavigation('reset')}>
+							{translate('timemanager', 'Current week')}
+						</button>
+					{/if}
+					<button class="next" on:click|preventDefault={() => weekNavigation('next')}>
+						{translate('timemanager', 'Next week')}
+					</button>
+				</span>
+			</nav>
+		{/if}
+	</div>
+</div>
