2d7eb39b1a9b56b390a9c7f9d884ff085ad36f06
Bot <mail@te-online.net>
2023-07-10 21:32:16 +0200

    Add release v1.1.16 of app files_linkeditor.

---
+++ files_linkeditor/v1.1.16/files_linkeditor/l10n/ast.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_linkeditor",
+    {
+    "The file is locked." : "El ficheru ta bloquiáu.",
+    "An internal server error occurred." : "Prodúxose un error internu del sirvidor.",
+    "An error occurred!" : "¡Prodúxose un error!"
+},
+"nplurals=2; plural=(n != 1);");
