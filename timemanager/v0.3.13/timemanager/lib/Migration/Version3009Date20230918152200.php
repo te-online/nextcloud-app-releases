aa65685eb466fac4358dcdf6ecbfc1f4a751d093
Bot <mail@te-online.net>
2024-03-30 12:30:34 +0100

    Add release v0.3.13 of app timemanager.

---
+++ timemanager/v0.3.13/timemanager/lib/Migration/Version3009Date20230918152200.php
@@ -0,0 +1,69 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace OCA\TimeManager\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version3009Date20230918152200 extends SimpleMigrationStep {
+	/** @var IDBConnection */
+	private $db;
+
+	public function __construct(IDBConnection $db) {
+		$this->db = $db;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array   $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array   $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable("timemanager_share");
+
+		$table->addColumn("recipient_id", "string", [
+			"notnull" => false,
+			"length"  => 64,
+		]);
+		$table->addColumn("recipient_type", "string", [
+			"notnull" => false,
+			"length"  => 64,
+		]);
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array   $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+		$query = $this->db->getQueryBuilder();
+		$query->update("timemanager_share")
+			->set("recipient_id", "recipient_user_id")
+			->set("recipient_type", $query->expr()->literal("user"));
+		$query->executeStatement();
+	}
+}
