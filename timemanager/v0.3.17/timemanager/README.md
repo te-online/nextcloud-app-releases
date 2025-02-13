f7fadab359c2a1b48e1f206c82c6983a7e26d7d3
Bot <mail@te-online.net>
2025-02-13 18:09:17 +0100

    Add release v0.3.17 of app timemanager.

---
+++ timemanager/v0.3.17/timemanager/README.md
@@ -0,0 +1,80 @@
+# TimeManager App
+
+A Nextcloud app to track time spent on work, life or anything in-between.
+
+_Looking for screenshots, official release packages or a place to leave a rating?_ 👉 [Visit the app on the Nextcloud Appstore](https://apps.nextcloud.com/apps/timemanager)
+
+## Features
+
+- 💅 Organize time entries based on tasks, projects and clients.
+- 🎤 Quickly record a time entry for your latest tasks.
+- ✅ Check or uncheck a time entry (e.g. to save whether you've billed it).
+- 🧑‍💻 Get summaries of your time spent on tasks, projects and clients.
+- 📊 Take a glance at simple, but useful statistics.
+- 📒 Create basic reports using filters, timerange selection and a simple graph.
+- 🤝 Track your time together with others by sharing a client and its associated projects and tasks (_web app only_).
+- 🕰 Work with start & end of time entry or duration.
+
+## Update strategy
+
+☝️ A few words on the state of this app:
+
+> [!Tip]
+> If you have a **feature request**, please read the following section before opening an issue or to understand why I may have closed your already opened issue.
+
+### Background
+
+- This Nextcloud app was originally developed by [me](https://github.com/te-online) for my own use
+- I've published it at some point to share with whoever could be interested
+- The codebase is by no means in perfect shape (that's why versions continue to be suffixed "Beta")
+- I know that contributing is not easy as it is right now
+
+> [!Important]
+> I consider the app **mostly feature-complete**
+
+### Long-term goals
+
+- I have no interest in adding massive new features to this app, nor do I intend to change it in any big way
+- With new features comes new responsibility to maintain those and I have no interest in that
+
+> [!Important]
+> My **main concern is to support the existing feature set** as best as I can for the foreseeable future. My responses to feature requests should be seen with this goal in mind
+
+### Conclusion
+
+You may have come here with lots of ideas in your head and now you read this. It may be a disappointment to you, and I'm sorry about that.
+
+If you absolutely need these features, you are welcome to fork the repository and make your own version ✨
+
+## Development
+
+The client-side JavaScript of this plugin uses ES6 features and needs to be transpiled for use in a browser. To run a watch command that automatically updates the `bundle.js` file when you make changes, execute `npm run dev`. To make a simple build, use `npm build`.
+
+Before building or development, dependencies need to be installed once by running `npm install`.
+
+## Automated testing
+
+Some end-to-end testing is done using [Cypress](https://www.cypress.io/). Tests currently cover basic features of the app and run in a headless Chrome browser on a containerized Nextcloud instance. For this repository tests run on [a mirror repository on Gitlab](https://gitlab.com/te-online/timemanager).
+
+To run tests locally, change into the `tests` directory. Before running or working on tests, run `npm install` to install dependencies. Make sure to create a `.env` file based on `.env.example` and a `cypress.env.json` based on `cypress.env.example.json`; values can be chosen freely, only make sure they align between the two files.
+
+To run the app, install podman and podman-compose on your machine.
+
+- Then in the `tests` directory run `podman-compose up`,
+- followed by `podman exec $(podman ps -qf "name=app") sh -c 'chown www-data:root custom_apps'`.
+- Wait for the app to be ready,
+- then run `podman exec -u www-data $(podman ps -qf "name=app") sh -c 'php -f ./occ app:disable firstrunwizard'`.
+- Then activate the app by running `podman exec -u www-data $(podman ps -qf "name=app") sh -c 'php -f ./occ app:enable timemanager'`
+- Finally, run `npm start` and use Cypress' UI to start running the tests included in the spec file.
+
+To shut down containers and **delete** the temporary volumes, run `podman-compose down -v` in a second terminal while you're in the same `tests` directory.
+
+Test cases might depend on running in a specific order. This means you might need to wind down your containers and start them again as described above when re-running tests.
+
+**New features** are supposed to be covered by end-to-end tests in a way that reflects their average usage.
+
+**Bugfixes** are supposed to include a test case that demonstrates the bug being fixed and prevent it to be re-introduced in the future.
+
+## Changelog
+
+The changelog is available [in the CHANGELOG file](https://github.com/te-online/timemanager/blob/main/CHANGELOG.md)
---
+++ timemanager/v0.3.17/timemanager/lib/Controller/TApiController.php
@@ -0,0 +1,319 @@
+<?php
+
+namespace OCA\TimeManager\Controller;
+
+use OCA\TimeManager\Db\Client;
+use OCA\TimeManager\Db\ClientMapper;
+use OCA\TimeManager\Db\ProjectMapper;
+use OCA\TimeManager\Db\TaskMapper;
+use OCA\TimeManager\Db\TimeMapper;
+use OCA\TimeManager\Db\CommitMapper;
+use OCA\TimeManager\Db\ShareMapper;
+use OCA\TimeManager\Db\StorageHelper;
+use OCA\TimeManager\Helper\UUID;
+use OCP\AppFramework\ApiController;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http;
+use OCP\IRequest;
+use OCP\IConfig;
+use Psr\Log\LoggerInterface;
+
+class TApiController extends ApiController {
+	/** @var ClientMapper mapper for client entity */
+	protected $clientMapper;
+	/** @var ProjectMapper mapper for project entity */
+	protected $projectMapper;
+	/** @var TaskMapper mapper for task entity */
+	protected $taskMapper;
+	/** @var TimeMapper mapper for time entity */
+	protected $timeMapper;
+	/** @var ClientMapper mapper for client entity */
+	protected $commitMapper;
+	/** @var ShareMapper mapper for client entity */
+	protected $shareMapper;
+	/** @var StorageHelper helper for working on the stored data */
+	protected $storageHelper;
+	/** @var string user ID */
+	protected $userId;
+	/** @var IConfig */
+	private $config;
+	/** @var LoggerInterface logger */
+	private $logger;
+
+	/**
+	 * constructor of the controller
+	 * @param string $appName the name of the app
+	 * @param IRequest $request an instance of the request
+	 * @param ClientMapper $clientMapper mapper for client entity
+	 * @param ProjectMapper $projectMapper mapper for project entity
+	 * @param TaskMapper $taskMapper mapper for task entity
+	 * @param TimeMapper $timeMapper mapper for time entity
+	 * @param CommitMapper $commitMapper mapper for commit entity
+	 * @param string $userId user id
+	 */
+	function __construct(
+		$appName,
+		$userId,
+		IRequest $request,
+		ClientMapper $clientMapper,
+		ProjectMapper $projectMapper,
+		TaskMapper $taskMapper,
+		TimeMapper $timeMapper,
+		CommitMapper $commitMapper,
+		ShareMapper $shareMapper,
+		IConfig $config,
+		LoggerInterface $logger
+	) {
+		parent::__construct($appName, $request);
+		$this->clientMapper = $clientMapper;
+		$this->projectMapper = $projectMapper;
+		$this->taskMapper = $taskMapper;
+		$this->timeMapper = $timeMapper;
+		$this->commitMapper = $commitMapper;
+		$this->shareMapper = $shareMapper;
+		$this->config = $config;
+		$this->userId = $userId;
+		$this->logger = $logger;
+		$this->storageHelper = new StorageHelper(
+			$this->clientMapper,
+			$this->projectMapper,
+			$this->taskMapper,
+			$this->timeMapper,
+			$this->commitMapper,
+			$this->shareMapper,
+			$this->config,
+			$userId
+		);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param string $name the name of the item
+	 * @param string $note the note of the item
+	 * @return DataResponse
+	 */
+	function post($name, $note) {
+		$client = new Client();
+		$client->setName($name);
+		$client->setNote($note);
+		$client->setUserId($this->userId);
+
+		$client = $this->clientMapper->insert($client);
+		return new DataResponse($client->toArray());
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param json $data
+	 * @return DataResponse
+	 */
+	function updateObjectsFromWeb($data, $lastCommit) {
+		return $this->updateObjects($data, $lastCommit);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 * @CORS
+	 *
+	 * @param json $data
+	 * @return DataResponse
+	 */
+	function updateObjects($data, $lastCommit) {
+		$this->logger->debug("New API request:", ["app" => "timemanager"]);
+		$this->logger->debug(json_encode($data), ["app" => "timemanager"]);
+		$this->logger->info("API Request with commit: " . $lastCommit, ["app" => "timemanager"]);
+		$entities = ["clients", "projects", "tasks", "times"];
+
+		if (!$data && !$lastCommit) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
+		if (empty($data)) {
+			return new DataResponse(json_encode(["error" => "Data is mandatory."]), Http::STATUS_NOT_ACCEPTABLE);
+		}
+
+		$noData = true;
+
+		foreach ($entities as $entity) {
+			if (
+				$data[$entity] == null ||
+				!is_array($data[$entity]["created"]) ||
+				!is_array($data[$entity]["updated"]) ||
+				!is_array($data[$entity]["deleted"])
+			) {
+				return new DataResponse(
+					$data[$entity]["created"] .
+						"Error, " .
+						$entity .
+						" with created, updated and deleted subarrays is mandatory.",
+					Http::STATUS_NOT_ACCEPTABLE
+				);
+			}
+			if (
+				count($data[$entity]["created"]) > 0 ||
+				count($data[$entity]["updated"]) > 0 ||
+				count($data[$entity]["deleted"]) > 0
+			) {
+				$noData = false;
+			}
+		}
+
+		$clientCommit = $lastCommit;
+		$commit = UUID::v4();
+
+		if (!$noData) {
+			foreach ($entities as $entity) {
+				// For all entities take the created objects
+				$created = $data[$entity]["created"];
+				// try to create object, if object already present -> move it to the changed array
+				foreach ($created as $object) {
+					// mark with current commit
+					$object["commit"] = $commit;
+					// Add or update object here.
+					$this->storageHelper->addOrUpdateObject($object, $entity);
+				}
+				// For all entities take the changed objects
+				$updated = $data[$entity]["updated"];
+				// if current object commit <= last commit delivered by client
+				foreach ($updated as $object) {
+					// mark with current commit
+					$object["commit"] = $clientCommit;
+					$object["desiredCommit"] = $commit;
+					// Add or update object here.
+					$this->storageHelper->addOrUpdateObject($object, $entity);
+				}
+				// For all entities take the deleted objects
+				$deleted = $data[$entity]["deleted"];
+				// if current object commit <= last commit delivered by client
+				foreach ($deleted as $object) {
+					// mark with current commit
+					$object["commit"] = $clientCommit;
+					$object["desiredCommit"] = $commit;
+					// Add or update object here.
+					$this->storageHelper->maybeDeleteObject($object, $entity);
+				}
+			}
+		}
+
+		$results = [];
+
+		foreach ($entities as $entity) {
+			$results[] = $this->storageHelper->getObjectsAfterCommit($entity, $clientCommit);
+		}
+
+		$response = ["data" => []];
+
+		$index = 0;
+		foreach ($entities as $entity) {
+			$response["data"][$entity] = $results[$index];
+			$index++;
+		}
+
+		if (!$noData) {
+			$response["commit"] = $commit;
+			$this->storageHelper->insertCommit($commit);
+			$this->logger->info("Sending response [omitted] and commit " . $commit, ["app" => "timemanager"]);
+		} else {
+			$lastCommit = $this->storageHelper->getLatestCommit();
+			$response["commit"] = $lastCommit;
+			$this->logger->info("Sending response [omitted] and commit " . $lastCommit, ["app" => "timemanager"]);
+		}
+
+		$this->logger->debug("Sending response... " . json_encode($response), ["app" => "timemanager"]);
+
+		return new JSONResponse($response);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 */
+	function getHoursInPeriodStats(
+		$start,
+		$end,
+		string $group_by = "none",
+		string $clients = null,
+		string $projects = null,
+		string $tasks = null,
+		string $status = null,
+		$shared = false,
+		string $userFilter = "",
+		string $timezone = "UTC"
+	) {
+		// Get possible task ids to filters for
+		$filter_tasks = $this->storageHelper->getTaskListFromFilters($clients, $projects, $tasks, $shared);
+
+		$includedAuthors = $userFilter && strlen($userFilter) > 0 ? explode(",", $userFilter) : [];
+
+		// Get all time entries for time period
+		$times = $this->timeMapper->findForReport($start, $end, $status, $filter_tasks, $shared, $timezone);
+		$times = array_filter($times, function ($time) use ($includedAuthors) {
+			// Find details for parents of time entry. If it doesn't exist, we should filter out time entry
+			$task = $this->taskMapper->getActiveObjectById($time->getTaskUuid(), true);
+
+			if (!$task) {
+				return false;
+			}
+
+			// Filter for author
+			if ($includedAuthors && is_array($includedAuthors) && !in_array($time->getUserId(), $includedAuthors)) {
+				return false;
+			}
+
+			return true;
+		});
+		$sum = 0;
+
+		// Calculate sum
+		$grouped = [];
+		$js_date_format = "";
+
+		foreach ($times as $time) {
+			$duration = $time->getDurationInHours();
+
+			// Group by date
+			if ($group_by === "day") {
+				$day = $time->getStartFormatted("Y-m-d", $timezone);
+				if (!isset($grouped[$day])) {
+					$grouped[$day] = 0;
+				}
+				$grouped[$day] += $duration;
+			} elseif ($group_by === "week") {
+				$week = $time->getStartFormatted("Y-W", $timezone);
+				if (!isset($grouped[$week])) {
+					$grouped[$week] = 0;
+				}
+				$grouped[$week] += $duration;
+			} elseif ($group_by === "month") {
+				$month = $time->getStartFormatted("Y-m", $timezone);
+				if (!isset($grouped[$month])) {
+					$grouped[$month] = 0;
+				}
+				$grouped[$month] += $duration;
+			} elseif ($group_by === "year") {
+				$year = $time->getStartFormatted("Y", $timezone);
+				if (!isset($grouped[$year])) {
+					$grouped[$year] = 0;
+				}
+				$grouped[$year] += $duration;
+			}
+			// Legacy: No grouping
+			$sum += $duration;
+		}
+
+		// Send JS date format for object.keys along with grouping
+		if ($group_by === "day") {
+			$js_date_format = "yyyy-MM-dd";
+		} elseif ($group_by === "week") {
+			$js_date_format = "yyyy-II";
+		} elseif ($group_by === "month") {
+			$js_date_format = "yyyy-MM";
+		} elseif ($group_by === "year") {
+			$js_date_format = "yyyy";
+		}
+
+		return new DataResponse(["total" => $sum, "grouped" => $grouped, "js_date_format" => $js_date_format]);
+	}
+}
