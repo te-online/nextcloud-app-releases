29f1e585a645a1706cec6d03d2e9feb5ca47ab6c
Thomas Ebert <thomas.ebert@te-online.net>
2025-02-13 18:08:15 +0100

    fix: Remove broken 0.3.17 version to prepare for re-release

--- timemanager/v0.3.17/timemanager/js/views/Overlay.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	export let loading = false;
-</script>
-
-<div class="oc-dialog-dim" />
-<div class={`oc-dialog ${loading ? 'icon-loading' : ''}`} style="position: fixed;">
-	<slot />
-</div>
